5f4eb4be8ec8641219aab5c2df6bcdebe8e61171
Mishko <mishko.meteor@gmail.com>
2016-02-07 15:14:35 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public/css/header.css
    #	public/css/main.css
    #	public/index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,28 @@
 var app = angular.module('msApp', []);
 
-app.controller('headerCtrl',['$scope', function ($scope) {
+app.controller('headerCtrl',['$scope','$window', function ($scope, $window) {
 
+  $scope.mainViewSelector = 'explore';
+  $scope.headerText = {
+    explore: 'stop reinventing the wheel. explore friends\' designs',
+    yourSpace: 'your legacy. one space.'
+  };
+
+  // allow user to toggle between the main views: {explore / your-space}
+  $scope.setMainView = function(view) {
+    $scope.mainViewSelector = view;
+  }
+
+// enable fixed header animation
+  $scope.shrinkTopHeader = false;
+
+  angular.element($window).bind("scroll", function() {
+     if (this.pageYOffset > 1) {
+         $scope.shrinkTopHeader = true;
+     } else {
+         $scope.shrinkTopHeader = false;
+     }
+    $scope.$apply();
+});
 
 }]);
